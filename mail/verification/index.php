db154dd145dc24364c7a9d3bd2408a3cf7d0ac90
skr1pmen <Kiberkot.v.roymenge@gmail.com>
2024-06-30 15:37:42 +0400

    Version 0.1.0

---
+++ mail/verification/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @var $code
+ */
+?>
+<div class="mail_page"
+     style="width: 100%; height: max-content; display: flex; flex-direction: column; justify-content: center; align-items: center; gap: 1rem">
+    <h1 style="font-size: 4rem; margin-top: 2rem;">Help City</h1>
+    <h1 style="font-size: 1.5rem;">Ваш код для подтверждения аккаунта:</h1>
+    <p style="font-size: 3rem; margin-bottom: 2rem"><?= $code ?></p>
+</div>
