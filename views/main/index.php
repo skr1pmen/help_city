50a1d3154078616b4c24185959f8cad106066819
skr1pmen <Kiberkot.v.roymenge@gmail.com>
2024-08-17 13:48:58 +0400

    Version 0.2.1

--- views/main/index.php
+++ views/main/index.php
@@ -11,6 +11,16 @@ use dosamigos\chartjs\ChartJs;
 ?>
 
 <div class="page">
+    <div class="welcomeBlock">
+        <div>
+            <img src="/images/only_logo.svg" alt="">
+            <h1>HelpCity</h1>
+            <span>Сделай свой город лучше сам!</span>
+        </div>
+        <img src="/images/7965ebb1-5036-4ada-bb03-8ca0c48e5a69-transformed.png" alt="">
+        <i class="fa-solid fa-angles-down"></i>
+    </div>
+
     <h2>Статистика за последние 7 дней</h2>
     <?= ChartJs::widget([
         'type' => 'line',
@@ -66,60 +76,4 @@ use dosamigos\chartjs\ChartJs;
         ],
     ]);
     ?>
-
-    <!--    <h2>Статистика за последние 7 дней</h2>-->
-    <!--    --><?php //= ChartJs::widget([
-    //        'type' => 'line',
-    //        'options' => [
-    //            'class' => 'line_grafic'
-    //        ],
-    //        'data' => [
-    //            'labels' => [Date('D', strtotime("-6 days")), Date('D', strtotime("-5 days")), Date('D', strtotime("-4 days")), Date('D', strtotime("-3 days")), Date('D', strtotime("-2 days")), Date('D', strtotime("-1 days")), Date("D")],
-    //            'datasets' => [
-    //                [
-    //                    'label' => "Заявка создана",
-    //                    'borderColor' => "rgba(255, 0, 0, 1)",
-    //                    'fill' => false,
-    //                    'data' => [65, 59, 90, 81, 56, 55, 40]
-    //                ],
-    //                [
-    //                    'label' => "Заявка обработана",
-    //                    'borderColor' => "rgba(255, 220, 0, 1)",
-    //                    'fill' => false,
-    //                    'data' => [28, 48, 40, 19, 96, 27, 100]
-    //                ],
-    //                [
-    //                    'label' => "Заявка в работе",
-    //                    'borderColor' => "rgba(0, 24, 255, 1)",
-    //                    'fill' => false,
-    //                    'data' => [8, 4, 50, 29, 56, 35, 67]
-    //                ],
-    //                [
-    //                    'label' => "Заявка решена",
-    //                    'borderColor' => "rgba(0, 255, 0, 1)",
-    //                    'fill' => false,
-    //                    'data' => [12, 48, 20, 11, 106, 57, 87]
-    //                ]
-    //            ]
-    //        ],
-    //        'clientOptions' => [
-    //            'legend' => [
-    //                'display' => true,
-    //                'position' => 'bottom',
-    //                'labels' => [
-    //                    'fontSize' => 12,
-    //                    'fontColor' => "#425062",
-    //                ]
-    //            ],
-    //            'tooltips' => [
-    //                'enabled' => true,
-    //                'intersect' => true
-    //            ],
-    //            'hover' => [
-    //                'mode' => false
-    //            ],
-    //
-    //        ],
-    //    ]);
-    //    ?>
 </div>
